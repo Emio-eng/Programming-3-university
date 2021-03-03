6514383db1f9a2a6a6ec601b6192e0e969766d39
emilioES2019 <79717228+emilioES2019@users.noreply.github.com>
2021-03-02 22:00:44 -0300

    Add files via upload

---
+++ ex6.hs
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+fatores :: Int -> [Int]
+fatores n = [x | x <- [1 .. n], n `mod` x == 0] 
+
+perfeitos :: Int -> [Int]
+perfeitos n = [x | x <- [ 1  .. n], x ==  sum ( tail(reverse(fatores x)))]
+
+main :: IO ()
+main = do
+    print "Digite um numero: "
+    numero' <- getLine
+    let numero = read numero' :: Int
+=======
+fatores :: Int -> [Int]
+fatores n = [x | x <- [1 .. n], n `mod` x == 0] 
+
+perfeitos :: Int -> [Int]
+perfeitos n = [x | x <- [ 1  .. n], x ==  sum ( tail(reverse(fatores x)))]
+
+main :: IO ()
+main = do
+    print "Digite um numero: "
+    numero' <- getLine
+    let numero = read numero' :: Int
+>>>>>>> e22a376c168bea477e13dbb9780895055d5b04da
+    print $perfeitos numero
